00f9177ef31e097cffba289a6c677706d2f85b94
webitproff <webitproff@gmail.com>
2026-07-01 21:09:51 +0300

    V.3.0.0 Combined data export from multiple related tables
    Updated plugin version and metadata for DB Structure Viewer.

--- dbviewstructure/dbviewstructure.global.php
+++ dbviewstructure/dbviewstructure.global.php
@@ -4,21 +4,23 @@
  * Hooks=global
  * [END_COT_EXT]
  */
- 
 /**
- * DB Structure Viewer plugin for Cotonti Siena v.0.9.26, PHP 8.4+, MySQL 8.0+
- * Filename: dbviewstructure.global.php
- * Purpose: Connect to hook "global" in Cotonti Core. Here is Required for Administration button after update plugin, else Administration button may be lost.
- * Date: 2026-02-02
+ * DB Structure Viewer plugin for Cotonti v.1+, PHP 8.4+, MySQL 8.0+
+ * Filename: plugins/dbviewstructure/lang/dbviewstructure.global.php
+ * Purpose: global including
+ * Date: 01 July 2026 
+ * 
+ * Source: https://github.com/webitproff/cot-dbviewstructure
+ * Page in Cotonti Marketplace: https://abuyfile.com/ru/market/cotonti/plugs/cot-plug-db-view-structure
+ * 
  * @package dbviewstructure
- * @version 2.0.0
- * @author webitproff https://github.com/webitproff/cot-dbviewstructure
- * @copyright Copyright (c) webitproff 2025
+ * @version 3.0.0
+ * @author webitproff
+ * @copyright Copyright (c) webitproff 2026 https://github.com/webitproff
  * @license BSD
  */
- 
-
 
 defined('COT_CODE') or die('Wrong URL.');
 
 require_once cot_langfile('dbviewstructure', 'plug');
+
